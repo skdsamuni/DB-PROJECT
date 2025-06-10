d800ad8c85a545825bab3660d5b752ff5df7ccf5
skdsamuni <bscs2312338@szabist.pk>
2025-06-10 19:20:02 +0000

    Save

--- MYAPI/db.js
+++ MYAPI/db.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
--- MYAPI/index.js
+++ MYAPI/index.js
@@ -1,14 +1,67 @@
-const express = require('express');      //Library import karwaiin hy jissey App.get wali cheezen istemaal hoo sakti hy
-const cors = require('cors');            //Library import karwaii hy backend of node.js server 
-const pool = require('./db');            //PostgreSQL se link karne ke liye
-require('dotenv').config();              //Authentication ky liye
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db'); // your db.js
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('NuFAD API is running');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Root test route
+
+app.get('/', async (req, res) => {
+    try {
+        res.json('Nuclear Bomb is ACTIVE!');
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
 });
+
+
+//Getting tables from PorteSQL
+
+const tables = [
+  'attack_types', 'attackers', 'audits', 'facilities', 'incident_attackers', 'maintenance_logs', 'materials',
+   'nuclear_devices', 'nuclear_incidents', 'personnel', 'security_incidents', 'uranium_stocks'
+];
+
+
+// Dynamically create GET routes
+for (const table of tables) {
+  app.get(`/${table}`, async (req, res) => {
+    try {
+      const result = await pool.query(`SELECT * FROM ${table}`);
+      res.json(result.rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+}
+
+
+//Count Routes
+const countTables = [
+  'attack_types', 'attackers', 'audits', 'facilities', 'incident_attackers',
+  'maintenance_logs', 'materials', 'nuclear_devices', 'nuclear_incidents',
+  'personnel', 'security_incidents', 'uranium_stocks'
+];
+
+// Dynamic GET count routes (keeps underscores in URLs)
+for (const table of countTables) {
+  app.get(`/total${table}`, async (req, res) => {
+    try {
+      const result = await pool.query(`SELECT COUNT(*) FROM ${table}`);
+      res.json(result.rows[0]); // returns { count: 'X' }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
